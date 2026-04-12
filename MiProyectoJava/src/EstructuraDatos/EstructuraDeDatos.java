f6c589efcdc38dd88aec9e728b323a3282d0e6eb
daniel donin <daniel donin@Daniel>
2026-04-12 00:11:06 -0500

    Mouredevjava

---
+++ MiProyectoJava/src/EstructuraDatos/EstructuraDeDatos.java
@@ -0,0 +1,20 @@
+package EstructuraDatos;
+
+
+public class EstructuraDeDatos {
+
+    
+    public static void main(String[] args) {
+        // ARRRAYS ------- arreglos
+        //Declaracion y creacion
+        int[]number= new int[3];     // aqui te dice que es una caja que puede almacenar 3 datos
+        System.out.println(number);
+        
+        String[]names={"Daniel","Ramos","Cervantes"};
+        System.out.println(names);
+                
+        //Acceso
+        System.out.println(names[1]);
+    }
+    
+}
